8a66b58650b3938ac6d821d850c07b5d971d7b54
Akbar Nurullayev <108784881+Nurullayev-Akbar@users.noreply.github.com>
2023-05-31 16:26:24 +0500

    filter lar qo'shildi

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,20 +2,58 @@ import { read, write } from "../utils/model.js";
 import jwt from "../utils/jwt.js";
 import { resolve } from "path";
 import { BadRequestError, InternalServerError } from "../utils/error.js";
+import { query } from "express";
 // import upload from "../utils/multer.js";
 
 const GET = (req, res, next) => {
   try {
-    if(req.query){
-      const postData = read("post_deploy");
-    }
-    
-
-    res.status(200).json({
+    const postData = read("post_deploy");
+    const sorov=Object.keys(req.query);
+    const {transfer_day ,  username , post_name , post_type }=req.query;
+    let filterData={}
+    if(sorov==0){
+      
+      res.status(200).json({
       status: 200,
       message: "zo'r",
       data: postData
     });
+    
+    }
+    else if (transfer_day){
+      
+       filterData=postData.filter(post=>post.transfer_day==transfer_day);
+    }
+    else if (username){
+      
+      filterData=postData.filter(post=>post.username.toLowerCase()==username.toLowerCase());
+   }
+   else if (post_name){
+      
+    filterData=postData.filter(post=>post.post_name.toLowerCase()==post_name.toLowerCase());
+ }
+  else if (post_type){
+      
+  filterData=postData.filter(post=>post.post_type.toLowerCase()==post_type.toLowerCase());
+}
+    
+    
+
+    
+
+
+
+      res.status(200).json({
+      status: 200,
+      message: "zo'r",
+      data: filterData
+    });
+    
+    
+    
+
+
+    
   } catch (error) {
     return next(error);
   }
@@ -38,7 +76,8 @@ const POST = (req, res, next) => {
     tashkilotchi,
     catigories,
     transfer_day,
-    transfer_time
+    transfer_time,
+    post_type
   } = req.body;
   const newPostData = {
     post_id: post.at(-1)?.post_id + 1 || 1,
@@ -50,7 +89,8 @@ const POST = (req, res, next) => {
     transfer_day,
     transfer_time,
     image,
-    status: false
+    status: false,
+    post_type
   };
 
   post.push(newPostData);
